7a73f6952b7661aed19823954c5ef4d38528303c
lteu <lteu@icloud.com>
2019-11-20 12:44:44 +0100

    message colored

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
+grn=$'\e[1;32m'
 red=$'\e[1;31m'
 end=$'\e[0m'
 DIRECTORY=~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 if [ -d "$DIRECTORY" ]; then
-    cp -r ./MiniZinc "$DIRECTORY"
+    if cp -r ./MiniZinc "$DIRECTORY"
+    then
+    	echo "\n${grn}Installed successfully!${end}\n"   
+	else [ $? != 0 ]                  
+   		echo "\n${red}An error occurred during installation${end}.\n"
+	fi
 else
     echo "\nInstallation ${red}FAILED${end}, path does not exist\n$DIRECTORY\n"
 fi
